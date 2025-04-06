d4e4b10eb335e23c309a4f37ed63febf7a2d196c
Diego Caruba <diego.caruba@suridata.com.br>
2025-04-06 20:09:56 -0300

    feat: implements authentication

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import studentRoutes from './routes/student.route';
+import authRoutes from './routes/auth.route';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -11,5 +12,6 @@ app.use(express.json());
 
 // Routes
 app.use('/api', studentRoutes);
+app.use('/api', authRoutes);
 
 export default app;
---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from 'express';
+import { loginSchema } from '../schemas/auth.schema';
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const prisma = new PrismaClient();
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, senha } = loginSchema.parse(req.body);
+
+    const student = await prisma.estudante.findUnique({ where: { email } });
+
+    if (!student) {
+      return res.status(401).json({ error: 'Email or password is invalid.' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(senha, student.senha);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: 'Email or password is invalid.' });
+    }
+
+    const token = jwt.sign(
+      { id: student.id, email: student.email },
+      process.env.JWT_SECRET!,
+      { expiresIn: '5m' }
+    );
+
+    return res.json({
+      token,
+      estudante: {
+        id: student.id,
+        nome: student.nome,
+        sobrenome: student.sobrenome,
+        email: student.email,
+      },
+    });
+  } catch (err: any) {
+    if (err.name === 'ZodError') {
+      return res.status(400).json({ error: err.errors });
+    }
+
+    return res.status(500).json({ error: 'Error during authentication.' });
+  }
+};
